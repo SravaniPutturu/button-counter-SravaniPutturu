636a6c7723235b82e78a6792278b76484ee9824c
Sravani <117716430+SravaniPutturu@users.noreply.github.com>
2023-05-10 19:55:58 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
-
-import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  var count = 0;
+  var counter = ()=>{
+    count++;
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        
+          <p>Button Clicked {count} 3 times</p>
+          <button onclick = {counter}>Click Me</button>
+        
     </div>
   )
 }
-
-export default App
