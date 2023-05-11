10b149aa83342329f88d18cd1dccc36dfaa2f854
Sravani <117716430+SravaniPutturu@users.noreply.github.com>
2023-05-11 22:29:42 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
 
-import React from "react";
+
+// import React from "react";
+import React, {useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  var [count,plusCount] = useState(0);
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>Button clicked {count} times</p>
+        <button onClick=(()=>plusCount(count + 1))>Click me</button>
     </div>
   )
 }
-
-export default App
